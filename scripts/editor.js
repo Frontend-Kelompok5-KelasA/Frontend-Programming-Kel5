5fd3456e59e57b28826596ccfe852bd492c2f13c
Natanael Gunawan <NatanaelGunawan73@gmail.com>
2026-09-13 16:38:47 +0700

    Typo error

--- scripts/editor.js
+++ scripts/editor.js
@@ -1,4 +1,4 @@
-import {connectNodes, registerComponent} from "../circuit.js";
+import {connectNodes, registerComponent} from "./circuit.js";
 
 const content = document.getElementsByClassName("content")[0];
 
