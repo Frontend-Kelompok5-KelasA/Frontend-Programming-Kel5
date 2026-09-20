e0753596fea428138f3bb1c5ce7b5ce5bf73856f
Natanael Gunawan <NatanaelGunawan73@gmail.com>
2026-09-20 15:43:41 +0700

    Moved Html and css to jquery folder

--- Kopi_Nusantara/Kopi_Nusantara_JQuery/script.js
+++ Kopi_Nusantara/Kopi_Nusantara_JQuery/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+    let txtHeader = $('.text-header'); 
+    
+    txtHeader.delay(500).animate({
+        opacity: 1,
+        width: '100%'
+    }, 1000);
+});
+
+$(document).ready(function() {
     let tanya = $('.question');
     let jawab = $('.answer');
 
